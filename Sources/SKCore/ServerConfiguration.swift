27315a70ed85fd43173a97419bf24a2b2e192330
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2019-01-11 19:58:47 +0100

    Fix header

--- Sources/SKCore/ServerConfiguration.swift
+++ Sources/SKCore/ServerConfiguration.swift
@@ -1,9 +1,14 @@
+//===----------------------------------------------------------------------===//
 //
-//  ServerConfiguration.swift
-//  SourceKitLSP
+// This source file is part of the Swift.org open source project
 //
-//  Created by Marcin Krzyzanowski on 11/01/2019.
+// Copyright (c) 2014 - 2019 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
 //
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
 
 import Foundation
 import Utility
