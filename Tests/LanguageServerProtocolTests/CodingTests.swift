4376582b0f0591281c7067a576ee4fc06dff62a8
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2019-07-16 13:35:58 +0200

    Remove language decoding check - no longer relevant

--- Tests/LanguageServerProtocolTests/CodingTests.swift
+++ Tests/LanguageServerProtocolTests/CodingTests.swift
@@ -203,7 +203,6 @@ final class CodingTests: XCTestCase {
     checkCoding(Language.objective_cpp, json: "\"objective-cpp\"")
     checkCoding(Language.swift, json: "\"swift\"")
     checkCoding(Language.unknown, json: "\"unknown\"")
-    checkDecoding(json: "\"asdfAF\"", expected: Language.unknown)
 
     checkCoding(DiagnosticCode.number(123), json: "123")
     checkCoding(DiagnosticCode.string("hi"), json: "\"hi\"")
