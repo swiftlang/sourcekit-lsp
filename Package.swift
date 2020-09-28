0326a363343b4d9d9aa842a58f554ca1c416ad55
Saleem Abdulrasool <compnerd@compnerd.org>
2020-09-28 14:59:36 -0700

    build: use Foundation for environment variable handling
    Adjust the environment variable check to be more portable by using
    Foundation rather than directly going to the C library which is spelt
    differently on each platform.

--- Package.swift
+++ Package.swift
@@ -220,13 +220,9 @@ let package = Package(
 // by the external environment. This allows sourcekit-lsp to take advantage of the automation used
 // for building the swift toolchain, such as `update-checkout`, or cross-repo PR tests.
 
-#if canImport(Glibc)
-import Glibc
-#else
-import Darwin.C
-#endif
+import Foundation
 
-if getenv("SWIFTCI_USE_LOCAL_DEPS") == nil {
+if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
   // Building standalone.
   package.dependencies += [
     .package(url: "https://github.com/apple/indexstore-db.git", .branch("main")),
