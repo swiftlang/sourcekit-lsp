802f8e7bcaf93359f173a6b19bef844ab2c44f44
Ben Langmuir <blangmuir@apple.com>
2021-04-06 16:26:53 -0700

    [sksupport] Remove unused old TSC argument parsing code
    Fixes deprecation warnings while building sourcekit-lsp. We haven't used
    this code since we switched to swift-argument-parser, so just delete it.

--- Sources/SKSupport/BuildConfiguration.swift
+++ Sources/SKSupport/BuildConfiguration.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Swift.org open source project
 //
-// Copyright (c) 2014 - 2019 Apple Inc. and the Swift project authors
+// Copyright (c) 2014 - 2021 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information
@@ -15,11 +15,4 @@ import TSCUtility
 public enum BuildConfiguration: String {
   case debug
   case release
-}
-
-extension BuildConfiguration: StringEnumArgument {
-  /// Type of shell completion to provide for this argument.
-  public static var completion: ShellCompletion {
-    return ShellCompletion.none
-  }
 }
