3647c4167c6fd2444e26044d9e5a9b4d955638d1
David Goldman <davg@google.com>
2022-06-08 11:25:03 -0400

    Make PathPrefixMapping fields public

--- Sources/SKCore/PathPrefixMapping.swift
+++ Sources/SKCore/PathPrefixMapping.swift
@@ -2,10 +2,10 @@ import Foundation
 
 public struct PathPrefixMapping {
   /// Path prefix to be replaced, typically the canonical or hermetic path.
-  let original: String
+  public let original: String
 
   /// Replacement path prefix, typically the path on the local machine.
-  let replacement: String
+  public let replacement: String
 
   public init(original: String, replacement: String) {
     self.original = original
