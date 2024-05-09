ed6163087586eb27382f693d1924aa6d8e882a05
Kim de Vos <kim.de.vos@vippsmobilepay.com>
2024-05-09 15:45:59 +0200

    Replace `IncrementalEdit` with `SourceEdit`

--- Sources/SourceKitLSP/Swift/SwiftLanguageService.swift
+++ Sources/SourceKitLSP/Swift/SwiftLanguageService.swift
@@ -537,7 +537,7 @@ extension SwiftLanguageService {
         keys.enableDiagnostics: 0,
         keys.syntacticOnly: 1,
         keys.offset: edit.range.lowerBound.utf8Offset,
-        keys.length: edit.length.utf8Length,
+        keys.length: edit.range.length.utf8Length,
         keys.sourceText: edit.replacement,
       ])
       do {
@@ -553,13 +553,7 @@ extension SwiftLanguageService {
     }
 
     let concurrentEdits = ConcurrentEdits(
-      fromSequential: edits.map {
-        IncrementalEdit(
-          offset: $0.range.lowerBound.utf8Offset,
-          length: $0.length.utf8Length,
-          replacementLength: $0.replacement.utf8.count
-        )
-      }
+      fromSequential: edits
     )
     await syntaxTreeManager.registerEdit(
       preEditSnapshot: preEditSnapshot,
