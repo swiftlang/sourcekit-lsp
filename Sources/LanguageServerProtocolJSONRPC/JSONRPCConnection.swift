8357ad0020cd22f606b4c9d029c89b6ed9f52967
Max Desiatov <m_desiatov@apple.com>
2024-09-22 17:03:06 +0100

    Revert "When a JSON-RPC connection is closed, send an error response to all outstanding requests"

--- Sources/LanguageServerProtocolJSONRPC/JSONRPCConnection.swift
+++ Sources/LanguageServerProtocolJSONRPC/JSONRPCConnection.swift
@@ -165,9 +165,6 @@ public final class JSONRPCConnection: Connection {
     ioGroup.notify(queue: queue) { [weak self] in
       guard let self = self else { return }
       Task {
-        for outstandingRequest in outstandingRequests.values {
-          outstandingRequest.replyHandler(LSPResult.failure(ResponseError.internalError("JSON-RPC Connection closed")))
-        }
         await self.closeHandler?()
         self.receiveHandler = nil  // break retain cycle
       }
