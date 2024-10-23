cbc8aed404d7429f95034fecbe576bfb13806222
Alex Hoppen <ahoppen@apple.com>
2024-10-23 09:34:40 -0700

    Guard access-level import by `compiler(>=6)`
    This allows us to build SourceKit-LSP using Swift 5.10 again.

--- Sources/SwiftExtensions/FileManagerExtensions.swift
+++ Sources/SwiftExtensions/FileManagerExtensions.swift
@@ -10,7 +10,11 @@
 //
 //===----------------------------------------------------------------------===//
 
+#if compiler(>=6)
 package import Foundation
+#else
+import Foundation
+#endif
 
 extension FileManager {
   /// Same as `fileExists(atPath:)` but takes a `URL` instead of a `String`.
