37aaa9218a8de3048323b872e4531e83607ca08b
Alex Hoppen <ahoppen@apple.com>
2025-02-27 19:04:26 -0800

    Mark `workspace/_sourceKitOptions` as freestanding
    The request doesn’t depend on any document state, so it should be freestanding instead of `workspaceRequest`.

--- Sources/SourceKitLSP/MessageHandlingDependencyTracker.swift
+++ Sources/SourceKitLSP/MessageHandlingDependencyTracker.swift
@@ -223,7 +223,7 @@ package enum MessageHandlingDependencyTracker: QueueBasedMessageHandlerDependenc
     case is ShutdownRequest:
       self = .globalConfigurationChange
     case is SourceKitOptionsRequest:
-      self = .workspaceRequest
+      self = .freestanding
     case is TriggerReindexRequest:
       self = .globalConfigurationChange
     case is TypeHierarchySubtypesRequest:
