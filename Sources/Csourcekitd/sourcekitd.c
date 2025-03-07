cf828a8135d6612606ab3fe80f950c96a3c322ec
Alex Hoppen <ahoppen@apple.com>
2025-03-06 22:01:00 -0800

    Consistently add license headers to all files
    To prepare for the GitHub Action license header check.

--- Sources/Csourcekitd/sourcekitd.c
+++ Sources/Csourcekitd/sourcekitd.c
@@ -1,1 +1,13 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
 // This file is here to prevent the package manager from warning about a target with no sources.
