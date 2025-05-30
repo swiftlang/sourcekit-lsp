696cb04f808fce6c505faea028df3910c6d30ca2
Evan Wilde <ewilde@apple.com>
2025-05-30 15:33:04 -0700

    FreeBSD: Fix setpriority call
    The setpriority function call is imported differently on FreeBSD than on
    Linux or macOS. No type conversions are needed.

--- Sources/TSCExtensions/Process+Run.swift
+++ Sources/TSCExtensions/Process+Run.swift
@@ -184,6 +184,8 @@ private func setProcessPriority(pid: Process.ProcessID, newPriority: TaskPriorit
   // can only be elevated, this means that we can effectively only change a process's priority once, when it is created.
   // All subsequent calls to `setpriority` will fail. Because of this, don't log an error.
   setpriority(PRIO_PROCESS, UInt32(pid), newPriority.posixProcessPriority)
+  #elseif os(FreeBSD)
+  setpriority(PRIO_PROCESS, pid, newPriority.posixProcessPriority)
   #else
   setpriority(__priority_which_t(PRIO_PROCESS.rawValue), UInt32(pid), newPriority.posixProcessPriority)
   #endif
