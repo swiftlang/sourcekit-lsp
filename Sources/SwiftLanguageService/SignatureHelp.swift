6f813e3865cd182b75d89018a43d8e4a4f9b4859
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2025-08-23 13:20:19 +0300

    Return signature help parameter offsets as UTF-16 offsets

--- Sources/SwiftLanguageService/SignatureHelp.swift
+++ Sources/SwiftLanguageService/SignatureHelp.swift
@@ -17,20 +17,43 @@ import SourceKitD
 import SourceKitLSP
 import SwiftBasicFormat
 
+fileprivate extension String {
+  func utf16Offset(of utf8Offset: Int, callerFile: StaticString = #fileID, callerLine: UInt = #line) -> Int {
+    guard
+      let stringIndex = self.utf8.index(self.startIndex, offsetBy: utf8Offset, limitedBy: self.endIndex)
+    else {
+      logger.fault(
+        """
+        UTF-8 offset is past the end of the string while getting UTF-16 offset of \(utf8Offset) \
+        (\(callerFile, privacy: .public):\(callerLine, privacy: .public))
+        """
+      )
+      return self.utf16.count
+    }
+    return self.utf16.distance(from: self.startIndex, to: stringIndex)
+  }
+}
+
 fileprivate extension ParameterInformation {
-  init?(_ parameter: SKDResponseDictionary, _ keys: sourcekitd_api_keys) {
+  init?(_ parameter: SKDResponseDictionary, _ signatureLabel: String, _ keys: sourcekitd_api_keys) {
     guard let nameOffset = parameter[keys.nameOffset] as Int?,
       let nameLength = parameter[keys.nameLength] as Int?
     else {
       return nil
     }
 
-    let documentation = parameter[keys.docComment].map {
-      StringOrMarkupContent.markupContent(MarkupContent(kind: .markdown, value: $0))
-    }
+    let documentation: StringOrMarkupContent? =
+      if let docComment: String = parameter[keys.docComment] {
+        .markupContent(MarkupContent(kind: .markdown, value: docComment))
+      } else {
+        nil
+      }
+
+    let labelStart = signatureLabel.utf16Offset(of: nameOffset)
+    let labelEnd = signatureLabel.utf16Offset(of: nameOffset + nameLength)
 
     self.init(
-      label: .offsets(start: nameOffset, end: nameOffset + nameLength),
+      label: .offsets(start: labelStart, end: labelEnd),
       documentation: documentation
     )
   }
@@ -45,11 +68,14 @@ fileprivate extension SignatureInformation {
     }
 
     let activeParameter = signature[keys.activeParameter] as Int?
-    let parameters = skParameters.compactMap { ParameterInformation($0, keys) }
+    let parameters = skParameters.compactMap { ParameterInformation($0, label, keys) }
 
-    let documentation = signature[keys.docComment].map {
-      StringOrMarkupContent.markupContent(MarkupContent(kind: .markdown, value: $0))
-    }
+    let documentation: StringOrMarkupContent? =
+      if let docComment: String = signature[keys.docComment] {
+        .markupContent(MarkupContent(kind: .markdown, value: docComment))
+      } else {
+        nil
+      }
 
     self.init(
       label: label,
