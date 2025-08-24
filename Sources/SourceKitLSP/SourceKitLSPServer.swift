d49bd988ff9f541b66874c195be9658f351b0c05
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2025-08-24 22:58:51 +0300

    Explain non-LSP compliant no active parameter trick and `:` retrigger in signature help

--- Sources/SourceKitLSP/SourceKitLSPServer.swift
+++ Sources/SourceKitLSP/SourceKitLSPServer.swift
@@ -1055,6 +1055,7 @@ extension SourceKitLSPServer {
       ? nil
       : LanguageServerProtocol.SignatureHelpOptions(
         triggerCharacters: ["(", "["],
+        // We retrigger on `:` as it's potentially after an argument label which can change the active parameter or signature.
         retriggerCharacters: [",", ":"]
       )
 
