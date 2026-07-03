d921ec7fbbebb02cc7d31273ce229f6276947762
Krishna Permi <40313959+krishnapermi@users.noreply.github.com>
2026-07-03 20:20:57 +0530

    Register FlipOperands in allSyntaxCodeActions

--- Sources/SwiftSyntaxCodeActions/SyntaxCodeActions.swift
+++ Sources/SwiftSyntaxCodeActions/SyntaxCodeActions.swift
@@ -27,6 +27,7 @@ package let allSyntaxCodeActions: [any SyntaxCodeActionProvider.Type] = {
     ConvertJSONToCodableStruct.self,
     ConvertStringConcatenationToStringInterpolation.self,
     ConvertZeroParameterFunctionToComputedProperty.self,
+    FlipOperands.self,
     FormatRawStringLiteral.self,
     MigrateToNewIfLetSyntax.self,
     OpaqueParameterToGeneric.self,
