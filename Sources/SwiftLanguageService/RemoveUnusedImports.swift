919878e5efc89dfa33e4f8fe5e039e4eb953c9ac
Alex Hoppen <ahoppen@apple.com>
2026-07-14 14:45:10 +0200

    Fix issues causing `Remove Unused Imports` to not show up in VS Code
    We had two issues here:
    1. VS Code does not show code actions with kind `sourceOrganizeImports` in the lightbulb menu. Change it to `empty` so that it does show up.
    2. We didn’t declare the command in `SwiftLanguageService`, so VS Code didn’t know to invoke SourceKit-LSP to perform the `remove.unused.imports.command`

--- Sources/SwiftLanguageService/RemoveUnusedImports.swift
+++ Sources/SwiftLanguageService/RemoveUnusedImports.swift
@@ -96,7 +96,7 @@ extension SwiftLanguageService {
     return [
       CodeAction(
         title: command.title,
-        kind: .sourceOrganizeImports,
+        kind: .refactor,
         diagnostics: nil,
         edit: nil,
         command: command.asCommand()
